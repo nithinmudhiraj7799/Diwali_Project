29b1780e947c38a9c87b8d2c8f836c19e60ccf46
nithinmudhiraj7799 <talarinithin7799@gmail.com>
2025-10-15 16:49:27 +0530

    hello

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Home from "./pages/Home";
+// import Home from "./pages/home";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 import Checkout from "./pages/checkout";
